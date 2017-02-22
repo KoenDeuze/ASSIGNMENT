60ccc51de0dc73c2dedfb9b8b1b680f29fb217db
Koen Deuze <koendeuze@gmail.com>
2017-02-22 12:03:24 +0100

    deployment 20170222_1202

---
+++ PLOT_5.R
@@ -0,0 +1,51 @@
+library(ggplot2)
+# read the 2 files
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# join the files
+DF <- merge(x = NEI, y = SCC, by = "SCC", all=T)
+
+# create a subset of the coal combustion-related sources
+BALT <- subset(DF, fips=="24510")
+
+# create a subset of the vehicle sources
+BALT_VEHICLES <- BALT[grep("Vehicles", BALT$EI.Sector), ]
+
+# create an aggregate (sum on emission) dataframe on sector and year
+BALT_VEHICLES_AGG <- aggregate(BALT_VEHICLES$Emissions, by=list(BALT_VEHICLES$EI.Sector, BALT_VEHICLES$year), FUN=sum)
+
+
+# set the columnnames
+names(BALT_VEHICLES_AGG) <- c("sector","year", "emission")
+
+# create the plot
+png("plot_5.png")
+options("scipen"=20)
+plot <- ggplot(data=BALT_VEHICLES_AGG, aes(x=year, y=emission, group=sector, colour=sector) ) 
+plot <- plot + geom_line()
+plot <- plot + xlab("year") + ylab("emission") + ggtitle("emission per sector per year") 
+plot
+dev.off()
+
+# alternative for all sectors combined
+
+# create an aggregate (sum on emission) dataframe on year
+BALT_VEHICLES_AGG <- aggregate(BALT_VEHICLES$Emissions, by=list(BALT_VEHICLES$year), FUN=sum)
+
+
+# set the columnnames
+names(BALT_VEHICLES_AGG) <- c("year", "emission")
+
+# create the plot
+png("plot_5_alternative.png")
+options("scipen"=20)
+plot <- ggplot(data=BALT_VEHICLES_AGG, aes(x=year, y=emission) ) 
+plot <- plot + geom_line()
+plot <- plot + xlab("year") + ylab("emission") + ggtitle("emission per year") 
+plot
+dev.off()
+
+
+
+
